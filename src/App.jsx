eb34491507abcd6d5b37cee578ccd9a0b89c9672
w4h4jx87 <wahajwahaj200@gmail.com>
2025-12-10 00:52:46 +0500

    k72

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
-import Home from './pages/home'
+import Home from './pages/Home'
 import Agence from './pages/Agence'
 import Project from './pages/Project'
 import Navbar from './Navbar/Navbar'
